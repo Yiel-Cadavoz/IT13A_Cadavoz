c4b4b37e869b21b97daefc217c4eda542b403893
Jabeniao <cadavoz.arrieldenver@gmail.com>
2025-02-11 08:15:36 +0800

    Arithmetic

--- Cadavoz_IT13A_Activities/src/prelim/Lab_Act1_Arithmetics.java
+++ Cadavoz_IT13A_Activities/src/prelim/Lab_Act1_Arithmetics.java
@@ -7,15 +7,15 @@ package prelim;
  */
 public class Lab_Act1_Arithmetics {
    public static void main(String[] args){
-        int firstnumber: 10;
-        int secondnumber: 4;
-        int thirdnumber: 6;
-        int fourthnumber: 3;
+        int firstnumber= 10;
+        int secondnumber= 4;
+        int thirdnumber= 6;
+        int fourthnumber= 3;
         
-        int firstoperation: firstnumber * secondnumber + thirdnumber;
-        int secondoperation: (firstnumber - secondnumber) % thirdnumber;
-        int thirdoperation: (firstnumber + secondnumber + thirdnumber) / fourthnumber;
-        int fourthoperation: firstnumber * thirdnumber - (secondnumber * secondnumber);
+        int firstoperation= firstnumber * secondnumber + thirdnumber;
+        int secondoperation= (firstnumber - secondnumber) % thirdnumber;
+        int thirdoperation= (firstnumber + secondnumber + thirdnumber) / fourthnumber;
+        int fourthoperation= firstnumber * thirdnumber - (secondnumber * secondnumber);
         
         System.out.println("10 * 4 + 6 = " + firstoperation);
         System.out.println("10 - 4 % 6 =" + secondoperation);
